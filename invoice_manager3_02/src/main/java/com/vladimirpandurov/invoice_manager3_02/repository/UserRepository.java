9a05f189fcb4a14da7110f16ee332aad88b4a5da
panvla <pandvlado@gmail.com>
2024-02-06 18:29:02 +0100

    role and user repository with create user feature added.

---
+++ invoice_manager3_02/src/main/java/com/vladimirpandurov/invoice_manager3_02/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.vladimirpandurov.invoice_manager3_02.repository;
+
+
+import com.vladimirpandurov.invoice_manager3_02.domain.User;
+
+import java.util.Collection;
+
+public interface UserRepository<T extends User> {
+
+    T create(T data);
+    Collection<T> list(int page,int pageSize);
+    T get(Long id);
+    T update(T data);
+    Boolean delete(Long id);
+
+}
