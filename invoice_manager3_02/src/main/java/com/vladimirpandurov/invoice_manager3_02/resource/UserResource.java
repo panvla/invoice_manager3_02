9c3d84135eea30afc906435e0fb16219963796af
panvla <pandvlado@gmail.com>
2024-02-07 10:45:44 +0100

    controller and custom HttpResponse class added and banner.txt

---
+++ invoice_manager3_02/src/main/java/com/vladimirpandurov/invoice_manager3_02/resource/UserResource.java
@@ -0,0 +1,46 @@
+package com.vladimirpandurov.invoice_manager3_02.resource;
+
+import com.vladimirpandurov.invoice_manager3_02.domain.HttpResponse;
+import com.vladimirpandurov.invoice_manager3_02.domain.User;
+import com.vladimirpandurov.invoice_manager3_02.dto.UserDTO;
+import com.vladimirpandurov.invoice_manager3_02.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<HttpResponse> saveUser(@RequestBody @Valid User user){
+        UserDTO userDTO = this.userService.createUser(user);
+        return ResponseEntity.created(getUri(userDTO.getId())).body(
+                HttpResponse.builder()
+                .timeStamp(LocalDateTime.now().toString())
+                .data(Map.of("user", userDTO))
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .message("User created")
+                .build()
+        );
+    }
+
+    private URI getUri(Long userId){
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/get/" + userId).toUriString());
+    }
+
+}
